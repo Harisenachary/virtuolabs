077ec618639fa289cac09d794543d05c4ffebe1c
Harisenachary <harisenacharypuppala@gmail.com>
2025-12-13 00:58:32 +0530

    Delete check_experiments.js

--- check_experiments.js
+++
@@ -1,6 +0,0 @@
-
-import { experiments } from './src/data/experiments.js';
-
-console.log("Experiments loaded successfully.");
-console.log("Count:", experiments.length);
-experiments.forEach(e => console.log("- " + e.id));
